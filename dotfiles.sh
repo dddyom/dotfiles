9c298c74ce8869e655b459644cf771bc422d7db5
dddyom <dddyom@gmail.com>
2022-02-25 10:38:26 +0600

    script dotfiles

---
+++ dotfiles.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+DIR=$HOME/diff/dotfiles
+
+DOTFILES=(
+	".bashrc"
+	".condarc"
+	".xinitrc"
+	".zshrc"
+
+	".config/betterlockscreenrc"
+	".config/alacritty"
+	".config/i3"
+	".config/nvim"
+
+	".config/polybar"
+	
+	".config/ranger"
+
+
+	".config/rofi"
+
+	".config/tmux"
+)
+
+for dotfile in "${DOTFILES[@]}";do
+	rm -rf "${HOME}/${dotfile}"
+	ln -sf "${DIR}/${dotfile}" "${HOME}/${dotfile}"
+done
